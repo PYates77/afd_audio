6da6ab543feb201a7bc0144440d59274ea167289
Paul Yates <paul@yates-engineering.com>
2026-08-16 23:49:02 -0600

    change AFD URL to Boulder

--- afd.sh
+++ afd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-URL="https://forecast.weather.gov/product.php?site=NWS&issuedby=FFC&product=AFD&glossary=1"
+URL="https://forecast.weather.gov/product.php?site=BOU&issuedby=BOU&product=AFD&format=txt&version=1&glossary=1"
 curl -s $URL | htmlq 'pre.glossaryProduct' --text > afd.txt
 awk -f afd.awk afd.txt
